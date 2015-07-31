ed41bda30d7f8670d44fd1acd4379cdb10a07088
Alex Beals <alexcbeals@gmail.com>
2015-07-31 01:37:52 -0400

    Added in support for multiple passwords

--- Tweak.xm
+++ Tweak.xm
@@ -1,25 +1,141 @@
 #import <substrate.h>
 #import <UIKit/UIKit.h>
 #import <CoreFoundation/CoreFoundation.h>
+#import <CoreFoundation/CFNotificationCenter.h>
 #include <libactivator/libactivator.h>
 
-// settings
+static NSString *eventFormat = @"com.hackingdartmouth.passcodeactivator.passcode"; 
+static NSString *prefPath = @"/var/mobile/Library/Preferences/com.hackingdartmouth.passcodeactivator.plist";
+
+@interface PasscodeEvent: NSObject <LAEventDataSource> {
+NSString *passcode;
+}
++ (void)load:(NSString *)code;
+@end
+
+@implementation PasscodeEvent
+
+PasscodeEvent *_passcodeEvent;
+
++ (void)load:(NSString *)code {
+	@autoreleasepool {
+		_passcodeEvent = [[PasscodeEvent alloc] init:code];
+	}
+}
+
+- (id)init:(NSString *)code {
+	if ((self = [super init])) {
+		passcode = code;
+		[LASharedActivator registerEventDataSource:self forEventName:[NSString stringWithFormat:@"%@-%@", eventFormat, passcode]];
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[LASharedActivator unregisterEventDataSourceWithEventName:[NSString stringWithFormat:@"%@-%@", eventFormat, passcode]];
+	[super dealloc];
+}
+
+- (NSString *)localizedTitleForEventName:(NSString *)eventName {
+	return @"Passcode Entered";
+}
+
+- (NSString *)localizedGroupForEventName:(NSString *)eventName {
+	return @"Passcode";
+}
+
+- (NSString *)localizedDescriptionForEventName:(NSString *)eventName {
+	return [[[[eventName componentsSeparatedByString:@"."] lastObject] componentsSeparatedByString:@"-"] lastObject];
+}
+
+- (BOOL)eventWithName:(NSString *)eventName isCompatibleWithMode:(NSString *)eventMode {
+	if ([eventMode isEqualToString:@"lockscreen"]) {
+		return true;
+	} else {
+		return false;
+	}
+}
+ 
+@end
+
 
 %hook SBDeviceLockController
-- (BOOL)attemptDeviceUnlockWithPassword:(NSString *)passcode appRequested:(BOOL)requested
-{
-	if (![passcode isKindOfClass:[NSString class]])
-		return %orig;
-	NSDictionary *prefs=[[NSDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.hackingdartmouth.passcodeactivator.plist"];
-	if ([[prefs objectForKey:@"enabled"] boolValue]) {
-		NSString *match=[prefs objectForKey:@"code"];
-		if ([match isEqualToString:passcode]) {
-			LAEvent *event = [LAEvent eventWithName:@"com.hackingdartmouth.passcodeactivator.passcode" mode:[LASharedActivator currentEventMode]];
-			[LASharedActivator sendEventToListener:event];
-		}
+
+- (BOOL)attemptDeviceUnlockWithPassword:(NSString *)passcode appRequested:(BOOL)requested {
+	BOOL response = %orig;
+	if (![passcode isKindOfClass:[NSString class]]) {
+		return response;
 	}
 
-	return %orig;
+	CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(),
+										 CFSTR("com.hackingdartmouth.passcodeactivator.activateEvent"), 
+										 NULL, 
+										 (__bridge CFDictionaryRef)@{@"passcode": passcode}, 
+										 kCFNotificationDeliverImmediately);
+
+	return response;
 }
 
 %end
+
+%hook SBUIPasscodeEntryField
+
+-(void)_resetForFailedPasscode:(BOOL)arg1 playUnlockFailedSound:(BOOL)arg2 {
+	if ([LASharedActivator hasEventWithName:[NSString stringWithFormat:@"%@-%@", eventFormat, [self stringValue]]]) {
+		// makes it so it doesn't vibrate if it does something
+		%orig(arg1, false);
+	} else {
+		%orig;
+	}
+}
+
+%end
+
+void updateEvents(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
+	NSDictionary *update = (__bridge NSDictionary*)userInfo;
+	if ([update objectForKey:@"remove"]) { // unassign the event, and unregister the source
+		LAEvent *event = [LAEvent eventWithName:[NSString stringWithFormat:@"%@-%@", eventFormat, [update objectForKey:@"remove"]] mode:@"lockscreen"];
+		[LASharedActivator unassignEvent:event];
+		[LASharedActivator unregisterEventDataSourceWithEventName:[NSString stringWithFormat:@"%@-%@", eventFormat, [update objectForKey:@"remove"]]];
+	} else if ([update objectForKey:@"old"]) { // create new one, get old and new events, remove all listeners from old, add all of them to new
+		[PasscodeEvent load:[update objectForKey:@"new"]];
+		LAEvent *oldEvent = [LAEvent eventWithName:[NSString stringWithFormat:@"%@-%@", eventFormat, [update objectForKey:@"old"]] mode:@"lockscreen"];
+		LAEvent *newEvent = [LAEvent eventWithName:[NSString stringWithFormat:@"%@-%@", eventFormat, [update objectForKey:@"new"]] mode:@"lockscreen"];
+		NSArray *assigned = [LASharedActivator assignedListenerNamesForEvent:oldEvent];
+		[LASharedActivator assignEvent:newEvent toListenersWithNames:assigned];
+		[LASharedActivator unassignEvent:oldEvent];
+		[LASharedActivator unregisterEventDataSourceWithEventName:[NSString stringWithFormat:@"%@-%@", eventFormat, [update objectForKey:@"old"]]];
+	} else {
+		[PasscodeEvent load:[update objectForKey:@"new"]];
+	}
+}
+
+void activateEvent(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
+	NSDictionary *eventData = (__bridge NSDictionary*)userInfo;
+
+	LAEvent *event = [LAEvent eventWithName:[NSString stringWithFormat:@"%@-%@", eventFormat, [eventData objectForKey:@"passcode"]] mode:[LASharedActivator currentEventMode]];
+	[LASharedActivator sendEventToListener:event];
+}
+
+%ctor {
+	// Construct notification listener
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDistributedCenter(),
+								NULL,
+								&updateEvents,
+								CFSTR("com.hackingdartmouth.passcodeactivator.updateEvents"),
+								NULL,
+								CFNotificationSuspensionBehaviorDeliverImmediately);
+
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDistributedCenter(),
+								NULL,
+								&activateEvent,
+								CFSTR("com.hackingdartmouth.passcodeactivator.activateEvent"),
+								NULL,
+								CFNotificationSuspensionBehaviorDeliverImmediately);
+
+	// Rebuild from file
+	NSMutableArray *rawSpecs = [[NSMutableArray alloc] initWithContentsOfFile:prefPath];
+	for (NSArray *rawSpec in rawSpecs) {
+		[PasscodeEvent load:[rawSpec objectAtIndex:1]];
+	}
+}
--- passcodeactivator/PasscodeActivator.mm
+++ passcodeactivator/PasscodeActivator.mm
@@ -1,16 +1,173 @@
 #import <Preferences/Preferences.h>
+#import <libactivator/libactivator.h>
+#import <CoreFoundation/CFNotificationCenter.h>
 
-@interface PasscodeActivatorListController: PSListController {
-}
+@interface PasscodeActivatorEditableListController: PSEditableListController
 @end
 
-@implementation PasscodeActivatorListController
-- (id)specifiers {
-	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"PasscodeActivator" target:self] retain];
+@implementation PasscodeActivatorEditableListController
+NSString *prefPath = @"/var/mobile/Library/Preferences/com.hackingdartmouth.passcodeactivator.plist";
+
+PSSpecifier *lastEdited = nil;
+ 
+extern NSString* PSDeletionActionKey;
+
+// -------------------
+//|	  Custom Methods  |
+// -------------------
+- (void) saveActivators {
+	NSMutableArray *specs = [[NSMutableArray alloc] init];
+	for (int i = 0; i < _specifiers.count; i++) {
+		PSSpecifier *spec = [_specifiers objectAtIndex:i];
+		if (spec.cellType == 4) {
+			NSString *name = [spec name];
+			NSString *value = [spec propertyForKey:@"value"];
+			NSArray *tempSpec = [[NSArray alloc] initWithObjects:name, value, nil];
+			[specs addObject:tempSpec];
+		}
 	}
+	[specs writeToFile:prefPath atomically:YES];
+}
+
+- (PSSpecifier *)createSpecNamed:(NSString *)name value:(NSString *)value {
+	PSSpecifier *tempSpec = [PSSpecifier preferenceSpecifierNamed:name
+		target:self
+		set:NULL
+		get:@selector(getValue:)
+		detail:Nil
+		cell:PSTitleValueCell
+		edit:Nil];
+	[tempSpec setProperty:value forKey:@"value"];
+	[tempSpec setButtonAction:@selector(editCode:)];
+	[tempSpec setProperty:NSStringFromSelector(@selector(deletedCode:)) forKey:PSDeletionActionKey];
+	return tempSpec;
+}
+
+- (NSString *)getValue: (PSSpecifier *) spec {
+    return [spec propertyForKey:@"value"];
+}
+
+- (void)addCode {
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Create Passcode"
+		message:@""
+		delegate:self
+		cancelButtonTitle:@"OK"
+		otherButtonTitles:nil];
+	alertView.tag = 2;
+	alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+	[alertView show];
+}
+
+- (void)editCode: (PSSpecifier *) spec {
+	lastEdited = spec;
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Edit Passcode"
+		message:@""
+		delegate:self
+		cancelButtonTitle:@"OK"
+		otherButtonTitles:nil];
+	alertView.tag = 2;
+	alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+	[[alertView textFieldAtIndex:0] setText:[NSString stringWithFormat:@"%@", [spec propertyForKey:@"value"]]];
+	[alertView show];
+}
+
+- (void) deletedCode:(PSSpecifier*)specifier {
+	NSMutableArray *itemsCopy = [_specifiers mutableCopy];
+	[itemsCopy removeObject:specifier];
+	_specifiers = [[NSArray arrayWithArray:itemsCopy] retain];
+
+	for (int i = 1; i < _specifiers.count; i++) {
+		[[_specifiers objectAtIndex:i] setName:[NSString stringWithFormat:@"Passcode %d", i]];
+	}
+
+	// Tell SpringBoard to update events
+	CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(),
+										 CFSTR("com.hackingdartmouth.passcodeactivator.updateEvents"), 
+										 NULL, 
+										 (__bridge CFDictionaryRef)@{@"remove": [specifier propertyForKey:@"value"]}, 
+										 kCFNotificationDeliverImmediately);
+
+	[self saveActivators];
+	[self reloadSpecifiers];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+	NSString *pass = [[alertView textFieldAtIndex:0] text];
+	NSDictionary *specData;
+	if ([alertView.title isEqualToString:@"Create Passcode"]) { // create passcode
+
+		// Adds a passcode object
+		NSMutableArray *specs = [(NSArray*)_specifiers mutableCopy];
+		PSSpecifier* tempSpec = [self createSpecNamed:[NSString stringWithFormat:@"Passcode %d", (int)[specs count]] value:pass];
+		[specs addObject:tempSpec];
+		_specifiers = [[NSArray arrayWithArray:specs] retain];
+		specData = @{@"new": pass}; //[NSDictionary dictionaryWithObject:pass forKey:@"new"];
+
+	} else { // save modified passcode
+		specData = @{@"new": pass, @"old": [lastEdited propertyForKey:@"value"]};
+		[lastEdited setProperty:pass forKey:@"value"];
+	}
+
+	// Tell SpringBoard to update events
+	CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(),
+										 CFSTR("com.hackingdartmouth.passcodeactivator.updateEvents"), 
+										 NULL, 
+										 (__bridge CFDictionaryRef)specData, 
+										 kCFNotificationDeliverImmediately);
+
+	[self saveActivators];
+	[self reloadSpecifiers];
+}
+
+// -------------------
+//|	 Override Methods |
+// -------------------
+
+- (id)specifiers {
+	if (_specifiers == nil) {
+		NSMutableArray *specs = [NSMutableArray array];
+		// initialize with group name
+		PSSpecifier* testSpecifier = [PSSpecifier preferenceSpecifierNamed:@"Passcode Activators"
+			target:self
+			set:NULL
+			get:NULL
+			detail:Nil
+			cell:PSGroupCell
+			edit:Nil];
+		[specs addObject:testSpecifier];
+		NSMutableArray *rawSpecs = [[NSMutableArray alloc] initWithContentsOfFile:prefPath];
+		for (NSArray *rawSpec in rawSpecs) {
+			PSSpecifier* tempSpec = [self createSpecNamed:[rawSpec objectAtIndex:0] value:[rawSpec objectAtIndex:1]];
+			[specs addObject:tempSpec];
+		}
+
+		_specifiers = [[NSArray arrayWithArray:specs] retain];
+	}
+
 	return _specifiers;
 }
-@end
 
-// vim:ft=objc
+- (void)_updateNavigationBar {
+	[super _updateNavigationBar];
+	UINavigationItem *nav = self.navigationItem;
+	NSMutableArray *barItems = [(NSArray *)nav.rightBarButtonItems mutableCopy];
+
+	if (barItems.count >= 1) {
+		UIBarButtonItem *currButton = [barItems objectAtIndex:0];
+		if ([currButton.title isEqualToString:@"Edit"] && barItems.count == 1) { // edit page: add
+			UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithTitle:@"Add" 
+					style:UIBarButtonItemStylePlain 
+					target:self
+					action:@selector(addCode)];
+			addButton.tag = 1;
+
+			[barItems addObject:addButton];
+		} else if ([currButton.title isEqualToString:@"Done"]) { // done page: remove
+			barItems = [NSMutableArray arrayWithObjects:currButton, nil];
+		}		
+	}
+
+	[nav setRightBarButtonItems:[[NSArray arrayWithArray:barItems] retain]];
+}
+
+@end
